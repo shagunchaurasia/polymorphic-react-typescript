9566380092176d3f01542b6c18448df0d30093f1
shagunchaurasia <chaurasia.shagun@gmail.com>
2022-08-28 21:28:50 +0900

    Finalized polymorphic component with ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
+import React, { useRef } from "react";
 import "./App.css";
 import Text from "./components/Text";
+
+const Emphasis = ({ children }: { children: string | number }) => {
+  return (
+    <em style={{ backgroundColor: "yellow", color: "black", fontSize: 40 }}>
+      {children}
+    </em>
+  );
+};
 function App() {
+
+  const ref = useRef<HTMLHeadingElement | null>(null);
+
   return (
     <div className="App">
-      <Text as="h1">This is Heading 1</Text>
+      <Text as="h1" color="yellow" style={{ backgroundColor: "red" }}>
+        This is Heading 1
+      </Text>
       <Text as="h2">This is Heading 2</Text>
       <Text as="h3">This is Heading 3</Text>
       <Text>This is span</Text>
+<Text as="h5" ref={ref} > This is a ref </Text>
+      <br />
+      <Text as={Emphasis}> This is important Emphasis</Text>
     </div>
   );
 }
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,15 +1,58 @@
 import React from "react";
 
-type TextProps<GenericComponent extends React.ElementType> = {
-  as?: GenericComponent;
-  children: React.ReactNode;
-} & React.ComponentPropsWithoutRef<GenericComponent>;
+type Rainbow =
+  | "violet"
+  | "indigo"
+  | "blue"
+  | "green"
+  | "yellow"
+  | "orange"
+  | "red";
 
-export const Text = <GenericComponent extends React.ElementType = "span">({
+type AsProp<C extends React.ElementType> = {
+  as?: C;
+};
+
+type PropsToOmit<
+  C extends React.ElementType,
+  Props
+> = keyof (AsProp<C> & Props);
+
+type PolymorphicComponentProps<
+  C extends React.ElementType,
+  Props = {}
+> = React.PropsWithChildren<Props & AsProp<C>> &
+  Omit<React.ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>;
+
+type TextProps = {
+  color?: Rainbow | "black";
+};
+
+type PolymorphicRef<C extends React.ElementType> = React.ComponentPropsWithRef<C>['ref'];
+
+
+type PolymorphicComponentPropsWithRef<C extends React.ElementType,P> = PolymorphicComponentProps<C,P> & {ref?:PolymorphicRef<C>}
+
+type TextComponent = <C extends React.ElementType>(props:PolymorphicComponentPropsWithRef<C,TextProps>)=> React.ReactElement| null;
+
+export const Text : TextComponent= React.forwardRef(<C extends React.ElementType = "span">({
   as,
+  style,
+  color,
   children,
   ...restProps
-}: TextProps<GenericComponent>) => {
+}: PolymorphicComponentProps<C, TextProps >, ref?:PolymorphicRef<C>) => {
   const Component = as || "span";
-  return <Component {...restProps}>{children}</Component>;
-};
+
+  const internalStyles = color
+    ? { style: { ...style, color } }
+    : style
+    ? { style: { ...style } }
+    : {};
+
+  return (
+    <Component {...restProps} {...internalStyles}>
+      {children} ref={ref}
+    </Component>
+  );
+})
