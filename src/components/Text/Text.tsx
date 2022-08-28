95ab34890299495fc14d99ba525d0a7b37ef58b5
shagunchaurasia <chaurasia.shagun@gmail.com>
2022-08-28 15:22:15 +0900

    Text Prop defined and default span case handled for type safety

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 
-export const Text = <GenericComponent extends React.ElementType>({
+type TextProps<GenericComponent extends React.ElementType> = {
+  as?: GenericComponent;
+  children: React.ReactNode;
+} & React.ComponentPropsWithoutRef<GenericComponent>;
+
+export const Text = <GenericComponent extends React.ElementType = "span">({
   as,
   children,
-}: {
-  as?: GenericComponent,
-  children: React.ReactNode;
-}) => {
+  ...restProps
+}: TextProps<GenericComponent>) => {
   const Component = as || "span";
-  return <Component >{children}</Component>;
+  return <Component {...restProps}>{children}</Component>;
 };
